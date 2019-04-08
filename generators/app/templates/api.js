c6b7457ebc2e88364b496ddb7cbbb9000458e17d
olliejm <olliejm@protonmail.com>
2019-04-08 16:49:34 +0100

    small changes

--- generators/app/templates/api.js
+++ generators/app/templates/api.js
@@ -5,9 +5,7 @@ const HttpAgent = require('agentkeepalive');
 const HttpsAgent = HttpAgent.HttpsAgent;
 
 function api(path, opts) {
-  if (typeof path !== 'string') {
-    return Promise.reject(new TypeError(`Expected \`path\` to be a string, got ${typeof path}`));
-  }
+  if (typeof path !== 'string') return Promise.reject(new TypeError(`Expected \`path\` to be a string, got ${typeof path}`));
 
   opts = Object.assign({
     json: true,
@@ -24,15 +22,11 @@ function api(path, opts) {
     'user-agent': 'adenin Digital Assistant, https://www.adenin.com/digital-assistant/'
   }, opts.headers);
 
-  if (opts.token) {
-    opts.headers.Authorization = `Bearer ${opts.token}`;
-  }
+  if (opts.token) opts.headers.Authorization = `Bearer ${opts.token}`;
 
   const url = /^http(s)\:\/\/?/.test(path) ? path : opts.endpoint + path;
 
-  if (opts.stream) {
-    return got.stream(url, opts);
-  }
+  if (opts.stream) return got.stream(url, opts);
 
   return got(url, opts).catch((err) => {
     throw err;
@@ -58,21 +52,5 @@ for (const x of helpers) {
   api[x] = (url, opts) => api(url, Object.assign({}, opts, {method}));
   api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, {method}));
 }
-
-api.handleError = function (error, authRequiresStatusCodes) {
-  return Activity.handleError(error, authRequiresStatusCodes);
-};
-
-api.isErrorResponse = function (response, successStatusCodes) {
-  return Activity.isErrorResponse(response, successStatusCodes);
-};
-
-api.dateRange = function (defaultRange) {
-  return Activity.dateRange(defaultRange);
-};
-
-api.pagination = function () {
-  return Activity.pagination();
-};
 
 module.exports = api;
